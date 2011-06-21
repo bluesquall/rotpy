186d85fe4d4a4d2c472b7df49a78cdd5a4ceb5a2
M J Stanway <m.j.stanway@alum.mit.edu>
2011-06-21 17:45:20 -0400

    basic distutils setup script

---
+++ setup.py
@@ -0,0 +1,8 @@
+from distutils.core import setup
+
+packlist = ['pyrotate', ]
+setup(name = 'pyrotate', version = '0.0.1',
+      description = 'Tools and examples for rotating things.',
+      author = 'M J Stanway', author_email = 'm.j.stanway@alum.mit.edu',
+      packages = packlist,
+    )
