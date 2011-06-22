9a2c0520ac1aef9a988be04d6e65bd4500b5e6a0
M J Stanway <m.j.stanway@alum.mit.edu>
2011-06-22 18:16:08 -0400

    cython comments for boundscheck and wraparound

--- rotpy/c_utility.pyx
+++ rotpy/c_utility.pyx
@@ -13,6 +13,9 @@ cimport cython
 from numpy cimport (ndarray, empty)
 #TODO determine if/when from-import is faster
 
+#cython: boundscheck=False
+#cython: wraparound=False
+
 cdef extern from "math.h":
     double sin(double)
     double cos(double)
@@ -34,7 +37,7 @@ def ixsea_rph_to_sname_Rvn(rph):
     cdef double cr = cos(rph[0]), cp = cos(rph[1]), ch = cos(rph[2])
     cdef ndarray[double, ndim=2, mode='c'] R = empty((3, 3))
 
-    #XXX as calculated in sage notebook
+    #XXX as calculated in sage notebook `IXSEA to SNAME'
     R[0,0] = ch*cp
     R[0,1] = -sp*sr*ch - sh*cr
     R[0,2] = -sp*ch*cr + sh*sr
@@ -44,9 +47,21 @@ def ixsea_rph_to_sname_Rvn(rph):
     R[2,0] = sp
     R[2,1] = sr*cp
     R[2,2] = cp*cr
-#    fpm2nwu = _to_matrix_intrinsic((r, p, -h), order = (1,2,3)) # heading flip
-#    fsk2fpm = np.array([[1,0,0],[0,-1,0],[0,0,-1]]) # SNAME to IXSEA
-#    nwu2ned = np.array([[1,0,0],[0,-1,0],[0,0,-1]]) # NWU to NED
     return R
+
+
+def ixsea_rph_to_sname_Rnv(rph):
+    """Rotation matrix from Euler angles--IXSEA to SNAME convention.
+ 
+    Parameters
+    ----------
+    rph
+
+    Returns
+    -------
+    Rnv
+
+    """
+    return ixsea_rph_to_sname_Rvn.transpose()
 
 
