32f26b0bfeb3c45df7c7a6028d412a145c8f6df5
M J Stanway <m.j.stanway@alum.mit.edu>
2011-06-22 15:32:09 -0400

    removed _to_matrix_intrinsic_old function (slightly slower according to ipython %timeit) and changed signs of matrix components to match convention in Fossen

--- rotpy/angles.py
+++ rotpy/angles.py
@@ -31,23 +31,13 @@ def to_matrix(angles, axes = (1, 2, 3), intrinsic=True,):
         raise NotImplementedError
 
     
-def _to_matrix_intrinsic_old(angles, axes=(1,2,3)):
-    #TODO confirm that this is actually the formulation for intrinsic
-    s1, s2, s3 = np.sin(angles)
-    c1, c2, c3 = np.cos(angles)
-    R1 = np.array([[1, 0, 0],[0, c1, -s1],[0, s1, c1]]) # e.g. roll
-    R2 = np.array([[c2, 0, s2],[0, 1, 0],[-s2, 0, c2]]) # e.g. pitch
-    R3 = np.array([[c3, -s3, 0],[s3, c3, 0],[0, 0, 1]]) # e.g. heading
-        #XXX the sign of the third angle (hdg) is reversed in IXSEA's docs
-    return reduce(np.dot, [R3, R2, R1])
-        # e.g. forward-port-mast to North-West-Up
-
 def _to_matrix_intrinsic(angles, axes=(1,2,3)):
     
     Rlist = [_to_matrix_about_elemental_axis(*aa) 
              for aa in reversed(zip(angles, axes))]
 #    Rlist.reverse() #XXX need to reverse the Rlist to apply in correct order
     return reduce(np.dot, Rlist)
+
 
 #TODO better terminology than elemental_axis
 def _to_matrix_about_elemental_axis(angle, axis):
@@ -61,17 +51,17 @@ def _to_matrix_about_elemental_axis(angle, axis):
 
 def _to_matrix_about_i(angle):
     s, c = np.sin(angle), np.cos(angle)
-    return np.array([[1, 0, 0],[0, c, -s],[0, s, c]]) # e.g. roll
+    return np.array([[1, 0, 0],[0, c, s],[0, -s, c]]) # e.g. roll
 
 
 def _to_matrix_about_j(angle):
     s, c = np.sin(angle), np.cos(angle)
-    return np.array([[c, 0, s],[0, 1, 0],[-s, 0, c]]) # e.g. pitch
+    return np.array([[c, 0, -s],[0, 1, 0],[s, 0, c]]) # e.g. pitch
 
 
 def _to_matrix_about_k(angle):
     s, c = np.sin(angle), np.cos(angle)
-    return np.array([[c, -s, 0],[s, c, 0],[0, 0, 1]]) # e.g. heading
+    return np.array([[c, s, 0],[-s, c, 0],[0, 0, 1]]) # e.g. heading
 
 
 def to_axis_angle(angles, order='xyz', intrinsic=True, ):
