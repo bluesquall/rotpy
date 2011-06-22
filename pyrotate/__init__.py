e8021c05f5cb2ac49ab1a71166641efed6cfc787
M J Stanway <m.j.stanway@alum.mit.edu>
2011-06-22 14:13:50 -0400

    added package __init__.py script

---
+++ pyrotate/__init__.py
@@ -0,0 +1,16 @@
+"""
+pyrotate
+========
+
+Python module for handling rotations.
+
+Optimized alternatives implemented in C will be loaded when present.
+
+"""
+
+try:
+    import c_angles as angles
+except ImportError:
+    import angles
+
+
