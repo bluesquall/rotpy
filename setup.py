0fdb84fc236b1909e5a1b79e2d037f4e129427d2
M J Stanway <m.j.stanway@alum.mit.edu>
2011-06-22 16:19:55 -0400

    modifued setup.py to build c_angles extension as well

--- setup.py
+++ setup.py
@@ -1,9 +1,21 @@
 from distutils.core import setup
+from distutils.extension import Extension
+from Cython.Distutils import build_ext
 
-packlist = ['rotpy', ]
-classifierlist = ['License :: OSI Approved :: MIT License',]
+import numpy
+
+c_angles_ext = Extension('rotpy.c_angles', ['rotpy/c_angles.pyx'],
+        include_dirs = [numpy.get_include()],
+    )
+
+package_list = ['rotpy', ]
+classifier_list = ['License :: OSI Approved :: MIT License',]
+ext_module_list = [c_angles_ext]
+
 setup(name = 'rotpy', version = '0.0.1',
       description = 'Tools and examples for rotating things.',
       author = 'M J Stanway', author_email = 'm.j.stanway@alum.mit.edu',
-      packages = packlist, classifiers = classifierlist,
+      packages = package_list, classifiers = classifier_list,
+      ext_modules = ext_module_list, 
+      cmdclass = {'build_ext': build_ext}
     )
