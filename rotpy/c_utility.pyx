5fc6420ebe5f046a1de325b5869c3f65a2021d94
M J Stanway <m.j.stanway@alum.mit.edu>
2011-07-06 17:43:01 -0400

    added utility functions to convert raw angles from internal TRDI sensors to rotation matrices

--- rotpy/c_utility.pyx
+++ rotpy/c_utility.pyx
@@ -19,6 +19,8 @@ cimport numpy as np
 cdef extern from "math.h":
     double sin(double)
     double cos(double)
+    double tan(double)
+    double atan(double)
 
 
 def ixsea_rph_to_sname_Rvn(rph):
@@ -63,5 +65,120 @@ def ixsea_rph_to_sname_Rnv(rph):
 
     """
     return ixsea_rph_to_sname_Rvn.transpose()
+
+
+def trdi_erph_to_rph(erph):
+    """Transform raw TRDI sensor angles to true Euler angles.
+
+    Transforms sensor angles (integer hundredths of degrees) to true 
+    Euler angles (radians), accounting for the internal tilt to gimbal 
+    angle caveat on TRDI attitude sensors.
+
+        P = arctan(tan(t1) * cos(R))
+
+    Parameters
+    ----------
+    erph : integer list
+
+    Returns
+    -------
+    rph : ndarray (3-by-1)
+
+    """
+    cdef double r = np.deg2rad(erph[0] / 100.) 
+    cdef double t = np.deg2rad(erph[1] / 100.)
+    cdef double p = atan(tan(t) * cos(r))
+    cdef double h = np.deg2rad(erph[2] / 100.)
+    # TODO consider faster method for deg2rad? e.g., from math.h import M_PI?
+    cdef np.ndarray[double, ndim=1, mode='c'] rph = np.array([r, p, h])
+    return rph
+
+
+def trdi_erph_to_Rin(erph):
+    """Rotation matrix from Euler angles--TRDI instrument to SNAME navigation.
+
+    This function transforms integer hundredths of degrees measured by the 
+    internal sensors on a Teledyne RD Instruments WorkHorse Doppler velocity
+    log (DVL) or acoustic Doppler current profiler (ADCP) into a rotation
+    matrix transforming instrument coordinates (starboard, forward, mast) 
+    to navigation coordinates in the SNAME convention (North, East, Down).
+
+    Parameters
+    ----------
+    erph : list of integers
+
+    Returns
+    -------
+    Rin : ndarray (3-by-3)
+
+    See Also
+    --------
+    trdi_erph_to_Rig : similar function that returns rotation matrix from
+        instrument coordinates to geographic (East, North, Up) coordinates.
+
+    """
+    cdef np.ndarray[double, ndim=1, mode='c'] rph = trdi_erph_to_rph(erph)
+    cdef double r = rph[0], p = rph[1], h = rph[2] # TODO avoid this
+    cdef double sr = sin(r), sp = sin(p), sh = sin(h)
+    cdef double cr = cos(r), cp = cos(p), ch = cos(h)
+    cdef np.ndarray[double, ndim=2, mode='c'] Rin = np.empty((3, 3))
+
+    #XXX    Taken from 'ADCP coordinate transformation' and modified 
+    #       to transform instrument to NED instead of ENU
+    #       Also confirmed in sagenb 'TRDI WHN to SNAME'
+    Rin[0,0] = sp * sr * ch - sh * cr
+    Rin[0,1] = ch * cp
+    Rin[0,2] = - sp * ch * cr - sh * sr
+    Rin[1,0] = sh * sp * sr + ch * cr
+    Rin[1,1] = sh * cp
+    Rin[1,2] = - sh * sp * cr + sr * ch
+    Rin[2,0] = sr * cp
+    Rin[2,1] = - sp
+    Rin[2,2] = - cp * cr
+    return Rin
+
+
+def trdi_erph_to_Rig(erph):
+    """Rotation matrix from Euler angles--TRDI instrument to geographic.
+
+    This function transforms integer hundredths of degrees measured by the 
+    internal sensors on a Teledyne RD Instruments WorkHorse Doppler velocity
+    log (DVL) or acoustic Doppler current profiler (ADCP) into a rotation
+    matrix transforming instrument coordinates (starboard, forward, mast) 
+    to geographic coordinates in the convention (East, North, Up).
+
+    Parameters
+    ----------
+    erph : list of integers
+
+    Returns
+    -------
+    Rig : ndarray (3-by-3)
+
+    See Also
+    --------
+    trdi_erph_to_Rin : similar function that returns rotation matrix from
+        instrument coordinates to SNAME (North, East, Down) coordinates.
+
+    """
+    cdef np.ndarray[double, ndim=1, mode='c'] rph = trdi_erph_to_rph(erph)
+    cdef double r = rph[0], p = rph[1], h = rph[2] # TODO avoid this
+    cdef double sr = sin(r), sp = sin(p), sh = sin(h)
+    cdef double cr = cos(r), cp = cos(p), ch = cos(h)
+    cdef np.ndarray[double, ndim=2, mode='c'] Rig = np.empty((3, 3))
+
+    #XXX    Taken from 'ADCP coordinate transformation'
+    #       Also confirmed in sagenb 'TRDI WHN to SNAME'
+    Rig[0,0] = sh * sp * sr + ch * cr
+    Rig[0,1] = sh * cp
+    Rig[0,2] = - sh * sp * cr + sr * ch
+    Rig[1,0] = sp * sr * ch - sh * cr
+    Rig[1,1] = ch * cp
+    Rig[1,2] = - sp * ch * cr - sh * sr
+    Rig[2,0] = - sr * cp
+    Rig[2,1] = sp
+    Rig[2,2] = cp * cr
+    return Rig
+
 
 
